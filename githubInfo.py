10cab2057f0aa1a532fba52b4f0e2b0379d2aea7
pouya fatol <pouya.fath@gmail.com>
2021-12-15 13:30:23 +0330

    add median

--- githubInfo.py
+++ githubInfo.py
@@ -2,7 +2,8 @@ from github import Github
 
 
 def getPublicRepositories(username):
-    token = "" # if you get exception on limit request please set your personal github token 
+    token = "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"
+    # token = "" # if you get exception on limit request please set your personal github token 
     if(token):
         github = Github(token)
     else:
@@ -15,37 +16,41 @@ def getPublicRepositories(username):
 
 
 def printTotalCountAndMean(repos):
-    dic = {'commits': 0, 'stars': 0, 'contributors': 0, 'branches': 0, 'tags': 0, 'forks': 0, 'releases': 0, 'closedIssues': 0, 'environments': 0}
+    dic = {'commits': [], 'stars': [], 'contributors': [], 'branches': [], 'tags': [], 'forks': [], 'releases': [], 'closedIssues': [], 'environments': []}
     languages = dict()
     for repo in repos:
         print("Full name:", repo.full_name)
-        dic['commits'] += repo.get_commits().totalCount
-        dic['stars'] += repo.stargazers_count
-        dic['contributors'] += repo.get_contributors().totalCount
-        dic['branches'] += repo.get_branches().totalCount
-        dic['tags'] += repo.get_tags().totalCount
-        dic['forks'] += repo.forks_count
-        dic['releases'] += repo.get_releases().totalCount
-        dic['closedIssues'] += repo.get_issues(state='closed').totalCount
-        dic['environments'] += repo.get_workflows().totalCount
+        dic['commits'].append(repo.get_commits().totalCount)
+        dic['stars'].append(repo.stargazers_count)
+        dic['contributors'].append(repo.get_contributors().totalCount)
+        dic['branches'].append(repo.get_branches().totalCount)
+        dic['tags'].append(repo.get_tags().totalCount)
+        dic['forks'].append(repo.forks_count)
+        dic['releases'].append(repo.get_releases().totalCount)
+        dic['closedIssues'].append(repo.get_issues(state='closed').totalCount)
+        dic['environments'].append(repo.get_workflows().totalCount)
         
         for language, codelines in repo.get_languages().items():
             if (language in list(languages.keys())):
-                languages[language] += int(codelines)
+                languages[language].append(int(codelines))
             else:
-                languages[language] = int(codelines)
+                languages[language] = [int(codelines)]
 
     print('-'*50)
     
     for key,value in dic.items():
-        mean = "{:.2f}".format(value/len(repos))
-        print(f'{key}: total = {value}, mean = {mean}')
+        value.sort()
+        mean = "{:.2f}".format(sum(value)/len(repos))
+        median = (value[int(len(value)/2 - 0.5)] + value[int(len(value)/2)])/2
+        print(f'{key}: all = {value}, total = {sum(value)}, mean = {mean}, median = {median}')
     
     print('-'*50)
 
-    for language, totalCodeLine in languages.items():
-        mean = "{:.2f}".format(totalCodeLine/len(repos))
-        print(f'{language}: total = {totalCodeLine}, mean = {mean}')
+    for language, codeLines in languages.items():
+        codeLines.sort()
+        mean = "{:.2f}".format(sum(codeLines)/len(repos))
+        median = (codeLines[int(len(codeLines)/2 - 0.5)] + codeLines[int(len(codeLines)/2)])/2
+        print(f'{language}: all = {codeLines}, total = {sum(codeLines)}, mean = {mean} , median = {median}')
     
 
 def run(account):
